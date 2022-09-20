3a4805e45ebd963cf8b5525c65698336b90cd548
aq032 <anthonyq032@gmail.com>
2022-09-20 10:04:22 -0700

    Added reactionSchema to Thought model

--- models/Thought.js
+++ models/Thought.js
@@ -1,29 +1,54 @@
-const { Schema, model, Types} = require("mongoose");
+const { Schema, model, Types } = require("mongoose");
+const moment = require("moment");
 
-const thoughtSchema = new Schema({
-  thoughtText: {
+const reactionSchema = new Schema({
+  reactionId: {
+    type: Schema.Types.ObjectId,
+    default: () => new Types.ObjectId()
+  },
+  reactionBody: {
     type: String,
     required: true,
-    minLength: 1,
     maxLength: 280
+  },
+  username: {
+    type: String,
+    required: true
   },
   createdAt: {
     type: Date,
     default: Date.now,
+    get: timeCreated => moment(timeCreated).format("MMM DD, YYYY [at] hh:mm a")
+  }
+},
+  {
+    toJSON: {
+      getters: true
+    }
+  }
+);
+
+const thoughtSchema = new Schema({
+  thoughtText: {
+    type: String,
+    minLength: 1,
+    maxLength: 280,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    required: true,
     get: (timeCreated) => moment(timeCreated).format("MMM DD, YYYY [at] hh:mm a")
   },
   username: [
     {
-      type: Schema.Types.ObjectId,
-      ref: "Thought"
+      type: Schema.Types.String,
+      ref: "User",
+      required: true
     }
   ],
-  reactions: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "User"
-    }
-  ]
+  reactions: [reactionSchema]
 },
   {
     toJSON: {
@@ -35,5 +60,9 @@ const thoughtSchema = new Schema({
 );
 
 const Thought = model("thought", thoughtSchema);
+
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
 
 module.exports = Thought;
